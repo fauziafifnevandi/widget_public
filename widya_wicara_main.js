bcd4db6165216c3210ac954c5c1a3a355b69302d
Andre Pradika Ekoputro Pratama <andrepradika@gmail.com>
2022-09-13 00:55:37 +0700

    Update widya_wicara_main.js

--- widya_wicara_main.js
+++ widya_wicara_main.js
@@ -1,23 +1,32 @@
+console.log("aku fauzi");
 const url = window.location.href;
-var api_endpoint = "http://127.0.0.1:8008/audio_widget_api";
-var myRequest = new Request(api_endpoint + "?url_source=" + url);
-console.log("awdhhuwas1");
+console.log(url);
+
+var myRequest = new Request("http://127.0.0.1:8008/audio_widget_api?url_source="+url);
+
 fetch(myRequest).then(function(response) {
-    if (response.status != "501") {
-        var ifr = document.querySelectorAll('div[class$="post-body entry-content float-container"]');
-        ifr.forEach(function(item) {
-            var iframe_elm = document.createElement("iframe");
-            iframe_elm.width = "100%";
-            iframe_elm.id = "iframe-audio-widget-widya-wicara"
-            iframe_elm.src = api_endpoint + "?url_source=" + url;
-            iframe_elm.frameBorder = "0";
-            iframe_elm.scrolling = "no";
-            iframe_elm.allowfullscreen = "";
-            iframe_elm.hidden = "";
-            iframe_elm.onload = "frameload()";
-            item.prepend(iframe_elm);
-        });
-    }
+  if (response.status != "501"){      
+  var ifr = document.querySelectorAll(
+    'div[class$="post-body entry-content float-container"]'
+  );
+  ifr.forEach(function (item) {
+    var iframe_elm = document.createElement("iframe");
+    iframe_elm.width = "100%";
+    iframe_elm.id = "iframe-audio-widget-widya-wicara"
+    // iframe_elm.src ="http://127.0.0.1:8008/audio_widget?audio_url=https://widya-tts-audio.s3.ap-southeast-3.amazonaws.com/andreganteng/e9a5fdd22e264f902a35bc75952acefd0738d001168932edfab85f353809e9f56060c570a67b95bc2b76f37f4d05d188_S0_Widya.wav";
+    iframe_elm.src ="http://127.0.0.1:8008/audio_widget_api?url_source="+url;
+    iframe_elm.frameBorder="0";
+    iframe_elm.scrolling="no";
+    iframe_elm.allowfullscreen="";
+    iframe_elm.hidden="";
+    iframe_elm.onload="frameload()";
+    item.prepend(iframe_elm);
+
+  });
+
+  }  
 }).catch(function(error) {
-    console.log(error);
+  console.log("error");
 });
+
+
