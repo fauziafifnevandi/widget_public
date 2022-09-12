d23b734aa3c7e1e934e6e77f3244fe0ff58ce4c7
Andre Pradika Ekoputro Pratama <andrepradika@gmail.com>
2022-09-13 00:40:14 +0700

    Update widya_wicara_main.js

--- widya_wicara_main.js
+++ widya_wicara_main.js
@@ -1,28 +1,25 @@
 const url = window.location.href;
 var api_endpoint = "http://127.0.0.1:8008/audio_widget_api";
-var myRequest = new Request(api_endpoint+"?url_source="+url);
+var myRequest = new Request(api_endpoint + "?url_source=" + url);
 console.log(ifr_s);
 console.log("awdhhuwas1");
 fetch(myRequest).then(function(response) {
-  if (response.status != "501"){      
-  var ifr = document.querySelectorAll(ifr_s);
-  console.log(ifr_s);
-  ifr.forEach(function (item) {
-    var iframe_elm = document.createElement("iframe");
-    iframe_elm.width = "100%";
-    iframe_elm.id = "iframe-audio-widget-widya-wicara"    
-    iframe_elm.src =api_endpoint+"?url_source="+url;
-    iframe_elm.frameBorder="0";
-    iframe_elm.scrolling="no";
-    iframe_elm.allowfullscreen="";
-    iframe_elm.hidden="";
-    iframe_elm.onload="frameload()";
-    item.prepend(iframe_elm);
-  });
-
-  }  
+    if (response.status != "501") {
+        var ifr = document.querySelectorAll(ifr_s);
+        console.log(ifr_s);
+        ifr.forEach(function(item) {
+            var iframe_elm = document.createElement("iframe");
+            iframe_elm.width = "100%";
+            iframe_elm.id = "iframe-audio-widget-widya-wicara"
+            iframe_elm.src = api_endpoint + "?url_source=" + url;
+            iframe_elm.frameBorder = "0";
+            iframe_elm.scrolling = "no";
+            iframe_elm.allowfullscreen = "";
+            iframe_elm.hidden = "";
+            iframe_elm.onload = "frameload()";
+            item.prepend(iframe_elm);
+        });
+    }
 }).catch(function(error) {
-  console.log("error");
+    console.log(error);
 });
-
-
