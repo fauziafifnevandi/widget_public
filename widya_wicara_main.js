bcc6418a7620c3c36f731eb3df6cc802e4822e83
Andre Pradika Ekoputro Pratama <andrepradika@gmail.com>
2022-09-13 01:09:04 +0700

    Update widya_wicara_main.js

--- widya_wicara_main.js
+++ widya_wicara_main.js
@@ -1,25 +1,21 @@
-console.log("aku fauzi");
+
 const url = window.location.href;
-console.log(url);
-
-var myRequest = new Request("http://127.0.0.1:8008/audio_widget_api?url_source="+url);
-
+var api_endpoint = "http://127.0.0.1:8008"
+var myRequest = new Request(api_endpoint+"/audio_widget_api?url_source="+url);
 fetch(myRequest).then(function(response) {
   if (response.status != "501"){      
   var ifr = document.querySelectorAll(ifrs);
   ifr.forEach(function (item) {
     var iframe_elm = document.createElement("iframe");
     iframe_elm.width = "100%";
-    iframe_elm.id = "iframe-audio-widget-widya-wicara"
-    // iframe_elm.src ="http://127.0.0.1:8008/audio_widget?audio_url=https://widya-tts-audio.s3.ap-southeast-3.amazonaws.com/andreganteng/e9a5fdd22e264f902a35bc75952acefd0738d001168932edfab85f353809e9f56060c570a67b95bc2b76f37f4d05d188_S0_Widya.wav";
-    iframe_elm.src ="http://127.0.0.1:8008/audio_widget_api?url_source="+url;
+    iframe_elm.id = "iframe-audio-widget-widya-wicara"    
+    iframe_elm.src =api_endpoint+"/audio_widget_api?url_source="+url;
     iframe_elm.frameBorder="0";
     iframe_elm.scrolling="no";
     iframe_elm.allowfullscreen="";
     iframe_elm.hidden="";
     iframe_elm.onload="frameload()";
     item.prepend(iframe_elm);
-
   });
 
   }  
