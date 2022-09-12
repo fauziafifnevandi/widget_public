6ce3047a3dee9372095ca02695108574754b7615
Andre Pradika Ekoputro Pratama <andrepradika@gmail.com>
2022-09-13 00:26:31 +0700

    Update widya_wicara_main.js

--- widya_wicara_main.js
+++ widya_wicara_main.js
@@ -1,7 +1,7 @@
 const url = window.location.href;
 var api_endpoint = "http://127.0.0.1:8008/audio_widget_api";
 var myRequest = new Request(api_endpoint+"?url_source="+url);
-
+console.log("awdhhuwa");
 fetch(myRequest).then(function(response) {
   if (response.status != "501"){      
   var ifr = document.querySelectorAll(ifr_s);
