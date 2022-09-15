edb7084bbcc169b43186b73229e6dca9875551b5
Andre Pradika Ekoputro Pratama <andrepradika@gmail.com>
2022-09-16 01:38:52 +0700

    Update widya_wicara_main.js

--- widya_wicara_main.js
+++ widya_wicara_main.js
@@ -1,26 +1,36 @@
 
-const url = window.location.href;
-var api_endpoint = "http://127.0.0.1:8008"
-var myRequest = new Request(api_endpoint+"/audio_widget_api?url_source="+url);
-fetch(myRequest).then(function(response) {
-  if (response.status != "501"){      
-  var ifr = document.querySelectorAll(ifrs);
-  ifr.forEach(function (item) {
-    var iframe_elm = document.createElement("iframe");
-    iframe_elm.width = "100%";
-    iframe_elm.id = "iframe-audio-widget-widya-wicara"    
-    iframe_elm.src =api_endpoint+"/audio_widget_api?url_source="+url;
-    iframe_elm.frameBorder="0";
-    iframe_elm.scrolling="no";
-    iframe_elm.allowfullscreen="";
-    iframe_elm.hidden="";
-    iframe_elm.onload="frameload()";
-    item.prepend(iframe_elm);
-  });
+const origin = window.location.href;
+var api_endpoint = "https://tts-audiowidget-api.widyawicara.dev"
+var url = api_endpoint+"/audio_widget_api?url_source="+origin;
 
-  }  
-}).catch(function(error) {
-  console.log("error");
-});
+var xhr = new XMLHttpRequest();
+xhr.open("GET", url);
+xhr.setRequestHeader("Accept", "application/json");
+xhr.setRequestHeader("Content-Type", "");
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+      if (xhr.status == 200) {
+        var ifr = document.querySelectorAll(ifrs);
+        ifr.forEach(function (item) {
+        var iframe_elm = document.createElement("iframe");
+        iframe_elm.width = "100%";
+        iframe_elm.id = "iframe-audio-widget-widya-wicara"    
+        var iframe_url = api_endpoint+"/audio_widget_api?url_audio="+xhr.responseText;    
+        iframe_elm.src = iframe_url;
+        iframe_elm.frameBorder="0";
+        iframe_elm.scrolling="no";
+        iframe_elm.allowfullscreen="";
+        iframe_elm.hidden="";
+        item.prepend(iframe_elm);
+      });
+      }
+    
+   }};
 
-
+  var data = `curl -X 'GET' \
+  '$url' \
+  -H 'accept: application/json' \
+  -d ''`.replace('$url', url);
+  xhr.send(data);
