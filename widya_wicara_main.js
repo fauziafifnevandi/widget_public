b5ca65ca70ab8216d8604a8f2b89d4d203b100a2
Andre Pradika Ekoputro Pratama <andrepradika@gmail.com>
2022-09-16 23:10:56 +0700

    Update widya_wicara_main.js

--- widya_wicara_main.js
+++ widya_wicara_main.js
@@ -1,36 +1,15 @@
-
-const origin = window.location.href;
-var api_endpoint = "https://tts-audiowidget-api.widyawicara.dev"
-var url = api_endpoint+"/audio_widget_api?url_source="+origin;
+var url = "https://tts-crawling.widyawicara.dev/api/v1/process?user_account_id=andreganteng&pitch=1&speed=1&languageCode=Id&speaker=Teguh&url_source=https%3A%2F%2Fandrepradika.blogspot.com%2F2022%2F09%2Fidxchannel-ikatan-dokter-anak-indonesia.html&mode=auto&widget=enable";
 
 var xhr = new XMLHttpRequest();
-xhr.open("GET", url);
-xhr.setRequestHeader("Accept", "application/json");
-xhr.setRequestHeader("Content-Type", "");
+xhr.open("POST", url);
+
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.setRequestHeader("Content-Length", "0");
+
 xhr.onreadystatechange = function () {
    if (xhr.readyState === 4) {
       console.log(xhr.status);
       console.log(xhr.responseText);
-      if (xhr.status == 200) {
-        var ifr = document.querySelectorAll(ifrs);
-        ifr.forEach(function (item) {
-        var iframe_elm = document.createElement("iframe");
-        iframe_elm.width = "100%";
-        iframe_elm.id = "iframe-audio-widget-widya-wicara"    
-        var iframe_url = api_endpoint+"/audio_widget_api?url_audio="+xhr.responseText;    
-        iframe_elm.src = iframe_url;
-        iframe_elm.frameBorder="0";
-        iframe_elm.scrolling="no";
-        iframe_elm.allowfullscreen="";
-        iframe_elm.hidden="";
-        item.prepend(iframe_elm);
-      });
-      }
-    
    }};
 
-  var data = `curl -X 'GET' \
-  '$url' \
-  -H 'accept: application/json' \
-  -d ''`.replace('$url', url);
-  xhr.send(data);
+xhr.send();
